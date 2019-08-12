425dbf561b98eb858c227dec6c8314abcb06fc43
mirmik <mirmikns@yandex.ru>
2019-08-12 23:30:56 +0300

    'a'

--- zenboards/charger_tp4056.py
+++ zenboards/charger_tp4056.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 import zencad
 
-class charger_tp4056(zencad.assemble.unit):
-	x = 25
-	y = 19.4
-	z = 1.3
+x = 25
+y = 19.4
+z = 1.3
 
-	hx = 9
-	hy = 7.5
-	hz = 3.7
-	hd = 2 
+hx = 9
+hy = 7.5
+hz = 3.7
+hd = 2 
 
+def charger_tp4056_model():
 	model = zencad.box(x,y,z)
 	model = model + zencad.box(hx,hy,hz).translate(x-hx+hd,y/2-hy/2,z)
 
-	def __init__(self):
-		super().__init__()
-		self.add_shape(self.model)
+	return model
 
+class charger_tp4056_unit(zencad.assemble.unit):
+	def __init__(self, socket=False, center=False):
+		super().__init__()
+
+		self.width = y
+		self.length = x
+		self.cheight = z + hz/2
+		self.height = z + hz
+
+		if socket:
+			self.model = charger_tp4056_model()
+			self.model = self.model.back(y/2).left(x).down(z + hz/2).rotateY(-zencad.deg(90)).rotateZ(-zencad.deg(90))
+			self.iobj = self.add_shape(self.model)
 
 
 if __name__ == "__main__":
-	charger = charger_tp4056()
+	charger = charger_tp4056_unit(socket=True)
 
 	zencad.disp(charger)
 	zencad.show()
